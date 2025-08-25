3d859ecf851585da96f7b0153b12dd5b25dce065
toffa <toffa@toffa.sec>
2025-08-25 17:14:52 +1000

    update main

--- cmd/cryphptor/main.go
+++ cmd/cryphptor/main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
+	"os"
+	"path/filepath"
+	"sync"
 
+	"github.com/DevPutat/cryphptor/internal/cryptolib"
 	"github.com/DevPutat/cryphptor/internal/scanner"
+	"golang.org/x/sync/semaphore"
 )
+
+const COUNT_GORUTINE = 100
 
 type arrayFlags []string
 
@@ -21,21 +29,60 @@ func (i *arrayFlags) Set(value string) error {
 var exclude arrayFlags
 
 func main() {
-	// TODO:: cli
-	p := flag.String("root", "./", "Path to root of project")
+	/*
+	* ========FLAGS======== -
+	* -key=encrypt-key-min-16-max-32
+	* -root=./path/to/project/abs/or/rel
+	* -dist=./path/to/dist/abs/or/rel
+	* -exclude=dir-or-file-name
+	* -exclude=/path/to/dir/or/file
+	* -exclude=its-is-slice
+	 */
+	key := flag.String("key", "", "Encryption key")
+	root := flag.String("root", "./", "Path to root of project")
+	dist := flag.String("dist", "./dist", "Output directory for encrypted files")
 	flag.Var(&exclude, "exclude", "Exclude paths: -exclude=dir1 -exclude=dir2")
 	flag.Parse()
-	conf := scanner.Config{RootDir: *p, Exclude: exclude}
-	fmt.Println(*p)
+
+	conf := scanner.Config{RootDir: *root, Exclude: exclude}
+	fmt.Println(*key, *dist)
 
 	s := scanner.NewScanner(conf)
 	list, err := s.Scan()
 	if err != nil {
-		fmt.Printf("ERROR: %v", err)
-		return
+		fmt.Printf("ERROR: %v\n", err)
+		os.Exit(1)
 	}
-	fmt.Println(len(list))
-	if len(list) < 100 {
-		fmt.Println(list)
+	if len(*key) < 16 {
+		fmt.Printf("ERROR: key is too short or empty: %s\n", *key)
+		os.Exit(1)
 	}
+	var wg sync.WaitGroup
+	sem := semaphore.NewWeighted(COUNT_GORUTINE)
+	ctx := context.Background()
+
+	enc := cryptolib.NewSafeEncryptor([]byte(*key))
+	for _, src := range list {
+		relPath, err := filepath.Rel(*root, src)
+		if err != nil {
+			fmt.Printf("ERROR: failed to compute relative path for %s: %v\n", src, err)
+			os.Exit(1)
+		}
+		dst := filepath.Join(*dist, relPath)
+
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			if err := sem.Acquire(ctx, 1); err != nil {
+				fmt.Printf("ERROR: failed to take gorutine %v\n", err)
+				os.Exit(1)
+			}
+			defer sem.Release(1)
+			if err := enc.Encrypt(src, dst); err != nil {
+				fmt.Printf("ERROR: failed to encrypt %s: %v\n", src, err)
+			}
+		}()
+	}
+	wg.Wait()
+	fmt.Println("Deployment completed")
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.24.2
 require (
 	github.com/stretchr/testify v1.10.0
 	golang.org/x/crypto v0.41.0
+	golang.org/x/sync v0.16.0
 )
 
 require (
